1e3d613093f6bb622c444df497b080850a1bc6a2
cokeIkigai <coke_vader@hotmail.com>
2025-12-05 11:53:19 +0100

    Create ReservaController for reservation management
    Implement ReservaController with service dependency.

---
+++ demo/service/demo/controller/demo/controller/ReservaController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public class ReservaController {
+
+    private final ReservaService reservaService;
+
+    public ReservaController(ReservaService reservaService) {
+        this.reservaService = reservaService;
+    }
+
+  // Devolver todas las reserva
+  // Devolver un reserva 
+  // añadir reserva
+  // modificar reserva
+  // borrarreserva
+}
